444dd15e48a7fd5b62d71edc950b13ebf043d3ec
Stepan Bubnov <exectrogod@gmail.com>
2020-11-29 21:22:32 +0300

    added invalid-data handler to registration fields

--- webapp/user/forms.py
+++ webapp/user/forms.py
@@ -1,6 +1,8 @@
 from flask_wtf import FlaskForm
 from wtforms import BooleanField, StringField, PasswordField, SubmitField
-from wtforms.validators import DataRequired, Email, EqualTo
+from wtforms.validators import DataRequired, Email, EqualTo, ValidationError
+
+from webapp.user.models import User
 
 
 class LoginForm(FlaskForm):
@@ -16,3 +18,14 @@ class RegistrationForm(FlaskForm):
     password = PasswordField('Пароль', validators=[DataRequired()], render_kw={'class': 'form_control'})
     password_2 = PasswordField('Повторите пароль', validators=[DataRequired(), EqualTo('password')], render_kw={'class': 'form_control'})
     submit = SubmitField('Отправить', render_kw={'class': 'btn btn-primary'})
+
+    def validate_user_name(self, user_name):
+        users_count = User.query.filter_by(user_name=user_name.data).count()
+        if users_count > 0:
+            raise ValidationError('Пользователь с таким именем уже существует.')
+
+    def validate_email(self, email):
+        users_count = User.query.filter_by(email=email.data).count()
+        if users_count > 0:
+            raise ValidationError('Пользователь с такой почтой уже существует.')
+
--- webapp/user/views.py
+++ webapp/user/views.py
@@ -60,5 +60,10 @@ def process_reg():
         db.session.commit()
         flash('Вы успешно зарегистировались!')
         return redirect(url_for('user.login'))
-    flash('Пожауйста, исправьте ошибки в форме.')
-    return redirect(url_for('user.register'))
+    else:
+        for field, errors in form.errors.items():
+            for error in errors:
+                flash('Ошибка в поле "{}": {}'.format(
+                    getattr(form, field).label.text, error
+                ))
+        return redirect(url_for('user.register'))
