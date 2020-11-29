f54946584c7f544c183127e365e7285a846fc677
Stepan Bubnov <exectrogod@gmail.com>
2020-11-29 16:29:57 +0300

    adden new blueprints, added new decorator, refactoring

--- webapp/admin/views.py
+++ webapp/admin/views.py
@@ -1,14 +1,12 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 
-from flask_login import current_user, login_required
+from webapp.user.decoratord import admin_required
 
 blueprint = Blueprint('admin', __name__, url_prefix='/admin')
 
 
 @blueprint.route('/')
-@login_required
+@admin_required
 def admin_index():
-    if current_user.is_admin:
-        return 'Hi Admin'
-    else:
-        return 'Ты не админ!'
+    title = 'Панель управления'
+    return render_template('admin/index.html', page_title=title)
